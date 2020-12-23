5cf74d5fcb732c7ec70de84ae2dce0fec8f6ecc3
ZoomIsland <zacherymilan@gmail.com>
2020-12-23 16:56:15 -0700

    Adding a little functionality to the show

--- src/components/ShowAppt.js
+++ src/components/ShowAppt.js
@@ -3,17 +3,41 @@ import react from 'react';
 import './ShowAppt.css';
 
 function ShowAppt(props) {
+  const getKeyword = () => {
+    if (props.appt.task === "Call") {
+      return "call";
+    } else {
+      return "reach out to";
+    }
+  }
+
+  const getIcon = () => {
+    switch(props.appt.task) {
+      case "Call":
+        return <i className="fas fa-phone-alt"></i>;
+      case "Manual task":
+        return <i className="fab fa-linkedin"></i>;
+      case "LinkedIn message":
+        return <i className="fab fa-linkedin"></i>;
+    }
+  }
+
   return (
     <div className={"apptShow " + (props.open ? "openAppt" : "")} style={props.style} onClick={props.toggleOpen}>
       <div className="icon">
-        {props.appt.task === "Call" && <i className="fas fa-phone-alt"></i>}
-        {props.appt.task === "Manual task" && <i className="fab fa-linkedin"></i>}
-        {props.appt.task === "LinkedIn message" && <i className="fab fa-linkedin"></i>}
+        {getIcon()}
       </div>
-      <div className="touchDetail">
-        <h3>Touch {props.appt.touchpoint} | {props.appt.notes}</h3>
-        <h4>{props.appt.task} {props.appt.task === "Manual task" && <span className="taskJoiner">with</span>} {props.appt.client}</h4>
-      </div>
+      {!props.open && 
+        <div className="touchDetail">
+          <h3>Touch {props.appt.touchpoint} | {props.appt.notes}</h3>
+          <h4>{props.appt.task} {props.appt.task === "Manual task" && <span className="taskJoiner">with</span>} {props.appt.client}</h4>
+        </div>
+      }
+      {props.open && 
+        <div className="touchQuestion">
+          <h4>Did you {getKeyword()} {props.appt.client}?</h4>
+        </div>
+      }
       <div className="respBtns">
         <div className="confirm"><i className="fas fa-check"></i></div>
         <div className="deny"><i className="fas fa-times"></i></div>
