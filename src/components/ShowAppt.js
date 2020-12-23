80fcadb952becc8e7d659bd812b8b848c02517a3
ZoomIsland <zacherymilan@gmail.com>
2020-12-23 16:14:27 -0700

    Main styling on ShowAppt done with some hover functionality.

--- src/components/ShowAppt.js
+++ src/components/ShowAppt.js
@@ -1,26 +1,25 @@
 import react from 'react';
 
+import './ShowAppt.css';
+
 function ShowAppt(props) {
-  const icon = () => {
-    if (props.appt.task === "Call") {
-      return "call"
-    }
-  }
   return (
-    <div className="apptShow">
+    <div className="apptShow" style={props.style}>
       <div className="icon">
-        {props.appt.task === "Call" && <p>Call</p>}
+        {props.appt.task === "Call" && <i className="fas fa-phone-alt"></i>}
+        {props.appt.task === "Manual task" && <i className="fab fa-linkedin"></i>}
+        {props.appt.task === "LinkedIn message" && <i className="fab fa-linkedin"></i>}
       </div>
       <div className="touchDetail">
-        <h3>{props.appt.touchpoint} | {props.appt.notes}</h3>
-  <h4>{props.appt.task} {props.appt.task === "Manual task" && <span className="taskJoiner">with</span>} {props.appt.client}</h4>
+        <h3>Touch {props.appt.touchpoint} | {props.appt.notes}</h3>
+        <h4>{props.appt.task} {props.appt.task === "Manual task" && <span className="taskJoiner">with</span>} {props.appt.client}</h4>
       </div>
       <div className="respBtns">
-        <button className="confirm">check</button>
-        <button className="deny">x</button>
+        <div className="confirm"><i className="fas fa-check"></i></div>
+        <div className="deny"><i className="fas fa-times"></i></div>
       </div>
       <div className="pushOver">
-        >
+        <i className="fas fa-chevron-right"></i>
       </div>
     </div>
   )
--- src/containers/ApptList.js
+++ src/containers/ApptList.js
@@ -3,11 +3,27 @@ import react from 'react';
 import ShowAppt from '../components/ShowAppt';
 
 function ApptList(props) {
+  function getStyle(index, length) {
+    if (index === 0) {
+      return {
+        borderRadius: "10px 10px 0px 0px",
+      }
+    } else if (index === length - 1) {
+      return {
+        borderRadius: "0px 0px 10px 10px",
+      }
+    } else {
+      return {
+        borderRadius: "0px",
+      }
+    }
+  }
+
   const timeMap = props.appts.map((timeSlot, timeIndex) => {
     return (
       <div className="timeContainer" key={timeIndex}>
         <h2>{timeSlot.time}</h2>
-        {timeSlot.appts.map((appt, apptIndex) => <ShowAppt appt={appt} key={apptIndex} />
+        {timeSlot.appts.map((appt, apptIndex) => <ShowAppt appt={appt} key={apptIndex} id={apptIndex} style={getStyle(apptIndex, timeSlot.appts.length)} />
         )}
       </div>
     )
